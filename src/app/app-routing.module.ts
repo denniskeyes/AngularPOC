4a188f8ca2873a7be36d7ba0cb673906f8371737
Dennis Keyes <dckeyes87@gmail.com>
2025-05-01 12:49:37 -0600

    Added routing to home page, removed angular app placeholder content, changed app-wide background color to #000000 (black)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    // Fallback/default route
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    // Redirect if user enters route that isn't defined here
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
